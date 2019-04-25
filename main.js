18b710cf18242eef68a51dee3efd8896bc0716b8
rocksride <ihavenonameTaras@gmail.com>
2019-04-25 11:33:07 +0300

    finished

--- main.js
+++ main.js
@@ -1,105 +1,146 @@
-TweenMax.from(".section-song", 2, {
-      width: "0%",
-      ease: Expo.easeInOut
-})
-
-TweenMax.from(".name", 2, {
-      delay: 2.5,
-      y: 20,
-      opacity: 0,
-      ease: Expo.easeInOut
-})
-
-TweenMax.from(".section-song__link", 2, {
-      delay: 2.8,
-      y: 20,
-      opacity: 0,
-      ease: Expo.easeInOut
-})
-
-TweenMax.from(".year", 2, {
-      delay: 2.8,
-      y: 20,
-      opacity: 0,
-      ease: Expo.easeInOut
-})
-
-TweenMax.from(".cover__song-name .heading-primary--main", 2, {
-      delay: 2.4,
-      y: 20,
-      opacity: 0,
-      ease: Expo.easeInOut
-})
-
-TweenMax.from(".cover__song-name .heading-primary--sub", 2, {
-      delay: 2.6,
-      y: 20,
-      opacity: 0,
-      ease: Expo.easeInOut
-})
-
-TweenMax.from(".details__main-heading", 2, {
-      delay: 3,
-      y: 20,
-      opacity: 0,
-      ease: Expo.easeInOut
-})
-TweenMax.from(".details__item:nth-child(1)", 2, {
-      delay: 3.2,
-      y: 20,
-      opacity: 0,
-      ease: Expo.easeInOut
-})
-TweenMax.from(".details__item:nth-child(2)", 2, {
-      delay: 3.4,
-      y: 20,
-      opacity: 0,
-      ease: Expo.easeInOut
-})
-TweenMax.from(".details__item:nth-child(3)", 2, {
-      delay: 3.6,
-      y: 20,
-      opacity: 0,
-      ease: Expo.easeInOut
-})
-TweenMax.from(".details__sub-heading", 2, {
-      delay: 3.6,
-      y: 20,
-      opacity: 0,
-      ease: Expo.easeInOut
-})
-
-TweenMax.staggerFrom(".details__bottom-item", 2, {
-      delay: 3.8,
+TweenMax.staggerFrom(".header__item", 2, {
       opacity: 0,
       y: 20,
       ease: Expo.easeInOut
 }, 0.1);
+// TweenMax.from(".section-song", 2, {
+//       width: "0%",
+//       ease: Expo.easeInOut
+// })
 
-TweenMax.from(".lower__heading .heading-primary--main", 2, {
-      delay: 4,
+TweenMax.from(".contact", 2, {
+      delay: 0.3,
       y: 20,
       opacity: 0,
       ease: Expo.easeInOut
 })
 
-TweenMax.from(".lower__heading .heading-primary--sub", 2, {
-      delay: 4.2,
-      y: 20,
+TweenMax.from(".quote", 2, {
+      delay: 0.3,
+      y: -20,
       opacity: 0,
       ease: Expo.easeInOut
 })
 
-TweenMax.from(".lower__icon", 2, {
-      delay: 4.4,
-      y: 20,
+TweenMax.from(".btn-plus", 2, {
+      delay: 0.7,
+      transform: 'translateX(-20px) rotate(90deg)',
       opacity: 0,
       ease: Expo.easeInOut
 })
 
-TweenMax.from(".lower .link", 2, {
-      delay: 4.4,
-      y: 20,
-      opacity: 0,
-      ease: Expo.easeInOut
+
+TweenMax.from(".year", 2, {
+    delay: 1,
+    x: -20,
+    opacity: 0,
+    ease: Expo.easeInOut
 })
+
+TweenMax.from(".line-1", 3, {
+    delay: 1.5,
+    opacity: 0,
+    x: -50,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".social-networks__item", 3, {
+    delay: 1.5,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+}, 0.1);
+
+// TweenMax.from(".section-song__link", 2, {
+//       delay: 2.8,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+
+// TweenMax.from(".year", 2, {
+//       delay: 2.8,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+
+// TweenMax.from(".cover__song-name .heading-primary--main", 2, {
+//       delay: 2.4,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+
+// TweenMax.from(".cover__song-name .heading-primary--sub", 2, {
+//       delay: 2.6,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+
+// TweenMax.from(".details__main-heading", 2, {
+//       delay: 3,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+// TweenMax.from(".details__item:nth-child(1)", 2, {
+//       delay: 3.2,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+// TweenMax.from(".details__item:nth-child(2)", 2, {
+//       delay: 3.4,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+// TweenMax.from(".details__item:nth-child(3)", 2, {
+//       delay: 3.6,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+// TweenMax.from(".details__sub-heading", 2, {
+//       delay: 3.6,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+
+// TweenMax.staggerFrom(".details__bottom-item", 2, {
+//       delay: 3.8,
+//       opacity: 0,
+//       y: 20,
+//       ease: Expo.easeInOut
+// }, 0.1);
+
+// TweenMax.from(".lower__heading .heading-primary--main", 2, {
+//       delay: 4,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+
+// TweenMax.from(".lower__heading .heading-primary--sub", 2, {
+//       delay: 4.2,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+
+// TweenMax.from(".lower__icon", 2, {
+//       delay: 4.4,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
+
+// TweenMax.from(".lower .link", 2, {
+//       delay: 4.4,
+//       y: 20,
+//       opacity: 0,
+//       ease: Expo.easeInOut
+// })
